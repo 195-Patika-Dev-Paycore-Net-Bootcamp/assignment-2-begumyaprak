a436f9a82af41a1b1e0011e3659767dc0839b045
Begüm Yaprak <begmyaprak@gmail.com>
2022-08-20 23:27:41 +0300

    00

--- BegumYaprakHW2/Model/StaffValidator.cs
+++ BegumYaprakHW2/Model/StaffValidator.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Transactions;
 using FluentValidation;
 using FluentValidation.Validators;
 
 namespace BegumYaprakHW2.Model
+
 {
-	public class StaffValidator:AbstractValidator<Staff>
+
+
+    public class StaffValidator:AbstractValidator<Staff>
 	{
 
 		public StaffValidator()
@@ -17,13 +21,13 @@ namespace BegumYaprakHW2.Model
             RuleFor(a => a.Name).NotEmpty().WithMessage("İsim boş olamaz").Length(3, 120);
             RuleFor(a => a.Lastname).NotEmpty().WithMessage("Soyisim boş olamaz").Length(3, 120);
 
+            RuleFor(a => a.Salary).NotEmpty().WithMessage("Gelir boş olamaz").GreaterThan(2000).LessThan(9000);
 
             RuleFor(a => a.Email).EmailAddress();
 
-            //RuleFor(a => a.PhoneNumber).NotEmpty().WithMessage("Telefon Numarası boş olamaz")
-            //.Matches(new Regex(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}")).WithMessage("PhoneNumber not valid");
+            
         }
-
+        
 
     }
 
